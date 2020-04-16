cf1aa5a00c0abd0f13107f2981cdb3fa5d1ddfd1
OctopusHugz <colsonscott53@gmail.com>
2020-04-16 19:03:03 +0000

    Changed _exit return from fork and execve error to -1

--- modules2.c
+++ modules2.c
@@ -66,7 +66,7 @@ int fork_exec(char *buf, char *path, char *av[4096], char *envp[])
 		buf = NULL;
 		free(path);
 		path = NULL;
-		_exit(EXIT_FAILURE);
+		_exit(-1);
 	}
 	else if (child_pid == 0)
 	{
@@ -77,7 +77,7 @@ int fork_exec(char *buf, char *path, char *av[4096], char *envp[])
 			buf = NULL;
 			free(path);
 			path = NULL;
-			_exit(EXIT_FAILURE);
+			_exit(-1);
 		}
 	}
 	else
